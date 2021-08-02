ef369ce2108e30137bf2bd97838238d497904df6
Bessolitsyn <anton.bessolitsyn@gmail.com>
2021-08-02 20:13:14 +0300

    First

---
+++ BessolitsynExcelToolAdd-in/RibbonBessolitsyn.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace BessolitsynExcelToolAdd_in
+{
+    partial class RibbonBessolitsyn : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public RibbonBessolitsyn()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.button4 = this.Factory.CreateRibbonButton();
+            this.button5 = this.Factory.CreateRibbonButton();
+            this.button6 = this.Factory.CreateRibbonButton();
+            this.button7 = this.Factory.CreateRibbonButton();
+            this.separator2 = this.Factory.CreateRibbonSeparator();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.button2);
+            this.group1.Items.Add(this.button3);
+            this.group1.Items.Add(this.button4);
+            this.group1.Items.Add(this.separator1);
+            this.group1.Items.Add(this.button5);
+            this.group1.Items.Add(this.button6);
+            this.group1.Items.Add(this.button7);
+            this.group1.Items.Add(this.separator2);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.Label = "button1";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Label = "SetRange1";
+            this.button2.Name = "button2";
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Label = "SetRange2";
+            this.button3.Name = "button3";
+            this.button3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button3_Click);
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // button4
+            // 
+            this.button4.Label = "Run";
+            this.button4.Name = "button4";
+            this.button4.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Label = "R1-R2";
+            this.button5.Name = "button5";
+            this.button5.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Label = "R2-R1";
+            this.button6.Name = "button6";
+            this.button6.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Label = "Intersection(R1,R2)";
+            this.button7.Name = "button7";
+            this.button7.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button7_Click);
+            // 
+            // separator2
+            // 
+            this.separator2.Name = "separator2";
+            // 
+            // RibbonBessolitsyn
+            // 
+            this.Name = "RibbonBessolitsyn";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.RibbonBessolitsyn_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button6;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button7;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator2;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal RibbonBessolitsyn RibbonBessolitsyn
+        {
+            get { return this.GetRibbon<RibbonBessolitsyn>(); }
+        }
+    }
+}
---
+++ BessolitsynExcelToolAdd-in/RibbonBessolitsyn.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace BessolitsynExcelToolAdd_in
+{
+    public partial class RibbonBessolitsyn
+    {
+        Excel.Worksheet ActiveSheet;
+        List<string> Range1, Range2;
+
+        // List as result of R1-R2
+        List<string> R1_R2 = new List<string>();
+
+        // List as result of R2-R1
+        List<string> R2_R1 = new List<string>();
+
+        //Общий диапазон
+        List<string> intersectionOf_R1_R2 = new List<string>();
+
+        private void RibbonBessolitsyn_Load(object sender, RibbonUIEventArgs e)
+        {
+            //var Window = Globals.ThisAddIn.Application.ActiveWindow;
+            //Excel.Workbook aWorkBook = Globals.ThisAddIn.Application.ActiveWorkbook;
+            //ActiveSheet = aWorkBook.ActiveSheet;
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, RibbonControlEventArgs e)
+        {
+            Range2 = RangeToList(Globals.ThisAddIn.Application.Selection as Excel.Range);
+
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            Range1 = RangeToList(Globals.ThisAddIn.Application.Selection as Excel.Range);
+
+        }
+        //Range processing
+        private void button4_Click(object sender, RibbonControlEventArgs e)
+        {
+
+            // List as result of R1-R2
+            List<string> R1_R2 = new List<string>();
+            R1_R2 = Range1;
+
+            // List as result of R2-R1
+            List<string> R2_R1 = new List<string>();
+
+            //Общий диапазон
+            List<string> intersectionOf_R1_R2 = new List<string>();
+
+            foreach (var item in Range2)
+            {
+
+                try
+                {
+                    if (R1_R2.Remove(item))
+                    {
+                        intersectionOf_R1_R2.Add(item);
+                    }
+                    else {
+                        R2_R1.Add(item);
+
+                    }
+
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.ToString());
+                }
+            }
+
+            
+
+            TextWriter w1 = new StreamWriter("R1_R2.txt");
+            foreach (String s in R1_R2)
+                w1.WriteLine(s);
+
+            w1.Close();
+
+            w1= new StreamWriter("R2_R1.txt");
+            foreach (String s in R2_R1)
+                w1.WriteLine(s);
+            w1.Close();
+
+            w1 = new StreamWriter("intersectionOf_R1_R2");
+            foreach (String s in R2_R1)
+                w1.WriteLine(s);
+
+            w1.Close();
+
+
+        }
+
+        private List<string> RangeToList(Excel.Range range)
+        {
+            List<string> result = new List<string>();
+            foreach (Excel.Range row in range.Rows)
+            {
+                var cell = (Excel.Range)row.Cells[1, 1];
+                result.Add(Convert.ToString(cell.Value2));
+            }
+            return result;
+        }
+
+        private void button7_Click(object sender, RibbonControlEventArgs e)
+        {
+            Openfile("intersectionOf_R1_R2");
+        }
+
+        private void button6_Click(object sender, RibbonControlEventArgs e)
+        {
+            Openfile("R2_R1.txt");
+        }
+
+        private void button5_Click(object sender, RibbonControlEventArgs e)
+        {
+            Openfile("R1_R2.txt");
+        }
+
+        public void Openfile(string file)
+        {
+            Process myProcess = new Process();
+            Process.Start(@"c:\Program Files\Notepad++\notepad++.exe", "\""+file+"\"");
+        }
+    }
+}
