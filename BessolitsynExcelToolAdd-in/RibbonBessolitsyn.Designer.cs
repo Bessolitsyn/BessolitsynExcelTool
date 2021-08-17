300060f7fd66efa84349befc8048b2e83ff551f7
Bessolitsyn <anton.bessolitsyn@gmail.com>
2021-08-17 12:37:58 +0300

    more a features

--- BessolitsynExcelToolAdd-in/RibbonBessolitsyn.Designer.cs
+++ BessolitsynExcelToolAdd-in/RibbonBessolitsyn.Designer.cs
@@ -35,17 +35,29 @@ namespace BessolitsynExcelToolAdd_in
         /// </summary>
         private void InitializeComponent()
         {
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl1 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl2 = this.Factory.CreateRibbonDropDownItem();
+            Microsoft.Office.Tools.Ribbon.RibbonDropDownItem ribbonDropDownItemImpl3 = this.Factory.CreateRibbonDropDownItem();
             this.tab1 = this.Factory.CreateRibbonTab();
             this.group1 = this.Factory.CreateRibbonGroup();
             this.button1 = this.Factory.CreateRibbonButton();
             this.button2 = this.Factory.CreateRibbonButton();
             this.button3 = this.Factory.CreateRibbonButton();
-            this.separator1 = this.Factory.CreateRibbonSeparator();
             this.button4 = this.Factory.CreateRibbonButton();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
             this.button5 = this.Factory.CreateRibbonButton();
             this.button6 = this.Factory.CreateRibbonButton();
             this.button7 = this.Factory.CreateRibbonButton();
             this.separator2 = this.Factory.CreateRibbonSeparator();
+            this.dropDown1 = this.Factory.CreateRibbonDropDown();
+            this.button8 = this.Factory.CreateRibbonButton();
+            this.separator3 = this.Factory.CreateRibbonSeparator();
+            this.button10 = this.Factory.CreateRibbonButton();
+            this.button9 = this.Factory.CreateRibbonButton();
+            this.separator4 = this.Factory.CreateRibbonSeparator();
+            this.button11 = this.Factory.CreateRibbonButton();
+            this.button12 = this.Factory.CreateRibbonButton();
+            this.button13 = this.Factory.CreateRibbonButton();
             this.tab1.SuspendLayout();
             this.group1.SuspendLayout();
             this.SuspendLayout();
@@ -68,6 +80,15 @@ namespace BessolitsynExcelToolAdd_in
             this.group1.Items.Add(this.button6);
             this.group1.Items.Add(this.button7);
             this.group1.Items.Add(this.separator2);
+            this.group1.Items.Add(this.dropDown1);
+            this.group1.Items.Add(this.button8);
+            this.group1.Items.Add(this.separator3);
+            this.group1.Items.Add(this.button10);
+            this.group1.Items.Add(this.button9);
+            this.group1.Items.Add(this.separator4);
+            this.group1.Items.Add(this.button11);
+            this.group1.Items.Add(this.button12);
+            this.group1.Items.Add(this.button13);
             this.group1.Label = "group1";
             this.group1.Name = "group1";
             // 
@@ -89,15 +110,15 @@ namespace BessolitsynExcelToolAdd_in
             this.button3.Name = "button3";
             this.button3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button3_Click);
             // 
-            // separator1
-            // 
-            this.separator1.Name = "separator1";
-            // 
             // button4
             // 
             this.button4.Label = "Run";
             this.button4.Name = "button4";
             this.button4.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button4_Click);
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
             // 
             // button5
             // 
@@ -120,6 +141,61 @@ namespace BessolitsynExcelToolAdd_in
             // separator2
             // 
             this.separator2.Name = "separator2";
+            // 
+            // dropDown1
+            // 
+            ribbonDropDownItemImpl1.Label = "R1-R2";
+            ribbonDropDownItemImpl2.Label = "R2-R1";
+            ribbonDropDownItemImpl3.Label = "Intersection";
+            this.dropDown1.Items.Add(ribbonDropDownItemImpl1);
+            this.dropDown1.Items.Add(ribbonDropDownItemImpl2);
+            this.dropDown1.Items.Add(ribbonDropDownItemImpl3);
+            this.dropDown1.Label = "dropDown1";
+            this.dropDown1.Name = "dropDown1";
+            // 
+            // button8
+            // 
+            this.button8.Label = "Paste results";
+            this.button8.Name = "button8";
+            this.button8.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button8_Click);
+            // 
+            // separator3
+            // 
+            this.separator3.Name = "separator3";
+            // 
+            // button10
+            // 
+            this.button10.Label = "Set Range with ID";
+            this.button10.Name = "button10";
+            this.button10.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Label = "find IDs";
+            this.button9.Name = "button9";
+            this.button9.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button9_Click);
+            // 
+            // separator4
+            // 
+            this.separator4.Name = "separator4";
+            // 
+            // button11
+            // 
+            this.button11.Label = "SetRange[A,B]";
+            this.button11.Name = "button11";
+            this.button11.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Label = "Get A ";
+            this.button12.Name = "button12";
+            this.button12.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Label = "Get B";
+            this.button13.Name = "button13";
+            this.button13.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button13_Click);
             // 
             // RibbonBessolitsyn
             // 
@@ -148,6 +224,15 @@ namespace BessolitsynExcelToolAdd_in
         internal Microsoft.Office.Tools.Ribbon.RibbonButton button6;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton button7;
         internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDown1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button9;
+        public Microsoft.Office.Tools.Ribbon.RibbonButton button8;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button10;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button11;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button12;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button13;
     }
 
     partial class ThisRibbonCollection
--- BessolitsynExcelToolAdd-in/RibbonBessolitsyn.cs
+++ BessolitsynExcelToolAdd-in/RibbonBessolitsyn.cs
@@ -15,7 +15,8 @@ namespace BessolitsynExcelToolAdd_in
     {
         Excel.Worksheet ActiveSheet;
         List<string> Range1, Range2;
-
+        List<KeyValuePair<string,string>> RangeWithIds;
+        List<KeyValuePair<string, string>> RangeAB;
         // List as result of R1-R2
         List<string> R1_R2 = new List<string>();
 
@@ -52,14 +53,14 @@ namespace BessolitsynExcelToolAdd_in
         {
 
             // List as result of R1-R2
-            List<string> R1_R2 = new List<string>();
+            R1_R2 = new List<string>();
             R1_R2 = Range1;
 
             // List as result of R2-R1
-            List<string> R2_R1 = new List<string>();
+            R2_R1 = new List<string>();
 
             //Общий диапазон
-            List<string> intersectionOf_R1_R2 = new List<string>();
+            intersectionOf_R1_R2 = new List<string>();
 
             foreach (var item in Range2)
             {
@@ -82,12 +83,33 @@ namespace BessolitsynExcelToolAdd_in
                 }
             }
 
-            
 
-            TextWriter w1 = new StreamWriter("R1_R2.txt");
+
+
+            string path = "R1_R2.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.Delete();
+            }
+            path = "R2_R1.txt";
+            fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.Delete();
+            }
+
+            path = "intersectionOf_R1_R2";
+            fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.Delete();
+            }
+
+
+            StreamWriter w1 = new StreamWriter("R1_R2.txt");
             foreach (String s in R1_R2)
                 w1.WriteLine(s);
-
             w1.Close();
 
             w1= new StreamWriter("R2_R1.txt");
@@ -96,9 +118,8 @@ namespace BessolitsynExcelToolAdd_in
             w1.Close();
 
             w1 = new StreamWriter("intersectionOf_R1_R2");
-            foreach (String s in R2_R1)
+            foreach (String s in intersectionOf_R1_R2)
                 w1.WriteLine(s);
-
             w1.Close();
 
 
@@ -110,7 +131,31 @@ namespace BessolitsynExcelToolAdd_in
             foreach (Excel.Range row in range.Rows)
             {
                 var cell = (Excel.Range)row.Cells[1, 1];
-                result.Add(Convert.ToString(cell.Value2));
+                
+                result.Add(Convert.ToString(cell.Value2)?.ToLower());
+            }
+            return result;
+        }
+        private List<KeyValuePair<string, string>> RangeWithIDsToList(Excel.Range range)
+        {
+            List<KeyValuePair<string, string>> result = new List<KeyValuePair<string, string>>();
+            foreach (Excel.Range row in range.Rows)
+            {
+                var cell1 = Convert.ToString(row.Cells[1, 1].Value2)?.ToLower();
+                var cell2 = Convert.ToString(row.Cells[1, 2].Value2);
+                result.Add(new KeyValuePair<string, string>(cell1, cell2));
+            }
+            return result;
+        }
+
+        private List<KeyValuePair<string, string>> RangeABToList(Excel.Range range)
+        {
+            List<KeyValuePair<string, string>> result = new List<KeyValuePair<string, string>>();
+            foreach (Excel.Range row in range.Rows)
+            {
+                var cell1 = Convert.ToString(row.Cells[1, 1].Text);
+                var cell2 = Convert.ToString(row.Cells[1, 2].Text);
+                result.Add(new KeyValuePair<string, string>(cell1, cell2));
             }
             return result;
         }
@@ -130,10 +175,127 @@ namespace BessolitsynExcelToolAdd_in
             Openfile("R1_R2.txt");
         }
 
+        private void button8_Click(object sender, RibbonControlEventArgs e)
+        {
+            //Выделить выбранный диапозон
+            int caseSwitch = dropDown1.SelectedItemIndex;
+            string marker = dropDown1.SelectedItem.Label;
+
+            switch (caseSwitch)
+            {
+                case 0:
+                    PasteResults(R1_R2, marker);
+                    break;
+                case 1:
+                    PasteResults(R2_R1, marker);
+                    break;
+                case 2:
+                    PasteResults(intersectionOf_R1_R2, marker);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
         public void Openfile(string file)
         {
             Process myProcess = new Process();
             Process.Start(@"c:\Program Files\Notepad++\notepad++.exe", "\""+file+"\"");
+        }
+        //find IDs
+        private void button9_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Range selection = Globals.ThisAddIn.Application.Selection as Excel.Range;
+            foreach (Excel.Range row in selection.Rows)
+            {
+                var cell = Convert.ToString(row.Cells[1, 1].Value2)?.ToLower();
+                if (RangeWithIds.Any(item=> item.Key==cell))
+                {
+                    (row.Cells[1, 2] as Excel.Range).Value = RangeWithIds.Single(item => item.Key == cell).Value;
+
+                }
+                else row.Cells[1, 2].Value= "not found";
+            }
+
+        }
+        //Set range with IDs
+        private void button10_Click(object sender, RibbonControlEventArgs e)
+        {
+            RangeWithIds = RangeWithIDsToList(Globals.ThisAddIn.Application.Selection as Excel.Range);
+        }
+
+        //set r[a,b]
+        private void button11_Click(object sender, RibbonControlEventArgs e)
+        {
+            RangeAB = RangeABToList(Globals.ThisAddIn.Application.Selection as Excel.Range);
+        }
+        //get a 
+        private void button12_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Range selection = Globals.ThisAddIn.Application.Selection as Excel.Range;
+            foreach (Excel.Range row in selection.Rows)
+            {
+                var cell = ((Excel.Range)row.Cells[1, 1]).Text;
+                if ((cell!= null ) && (RangeAB.Any(item => item.Value == cell)))
+                {
+                    try
+                    {
+
+                    (row.Cells[1, 1] as Excel.Range).Value = RangeAB.Single(item => item.Value!=null && item.Value == cell).Key;
+                    }
+                    catch (Exception)
+                    {
+                    string message = "Найдены значения:";
+                    RangeAB.Where(item => item.Value != null && item.Value == cell).ToList().ForEach(i => message = message + " " + i.Key);
+                    MessageBox.Show(message);
+                    }
+
+                }
+            
+            }
+        }
+        //get b
+        private void button13_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Range selection = Globals.ThisAddIn.Application.Selection as Excel.Range;
+            foreach (Excel.Range row in selection.Rows)
+            {
+                var cell = (string)((Excel.Range)row.Cells[1, 1]).Text;
+                if ((cell != null) && (RangeAB.Any(item => item.Key == cell)))
+                {
+                    try
+                    {
+                        (row.Cells[1, 1] as Excel.Range).Value = RangeAB.Single(item => item.Key != null && item.Key == cell).Value;
+                    }
+                    catch (Exception)
+                    {
+                        string message = "Найдены значения:";
+                        RangeAB.Where(item => item.Key != null && item.Key == cell).ToList().ForEach(i=> message = message + " " + i.Value);
+                        MessageBox.Show(message);
+                    }
+
+                }
+            }
+        }
+
+        public void PasteResults(List<string> rangeList, string marker)
+        {
+            
+            var selection = Globals.ThisAddIn.Application.Selection as Excel.Range;
+            foreach (Excel.Range row in selection.Rows)
+            {
+                var cell = (Excel.Range)row.Cells[1, 1];
+                var celLValue = Convert.ToString(cell?.Value2)?.ToLower();
+                if (celLValue != null)
+                {
+                    if (rangeList.Contains(celLValue))
+                    {
+                    (row.Cells[1, 2] as Excel.Range).Value = marker;
+
+                }
+                }
+            }
         }
     }
 }
